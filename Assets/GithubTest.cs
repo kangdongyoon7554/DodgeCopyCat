d390c428bd450725432921ea6aba6bb155811d85
Youn <yys0229@gmail.com>
2025-09-10 10:23:32 +0900

    202509101022_Init
    깃허브 연동 테스트

---
+++ Assets/GithubTest.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GithubTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // 안녕하세요. 강동윤입니다.
+        // 지금은 깃허브 연동 테스트 중입니다.
+        // 연동이 잘된다면 깃허브 공간에서도 해당 내용이 보이겠죠?!
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
